5c40e57e3415a978456e019aba72dd3f0765c663
Elle Machado Wolfgher <ewolfghe@c1r5p2.42sp.org.br>
2023-04-07 18:23:38 -0300

    export is ready biatch

--- sources/builtins/ft_command_unset.c
+++ sources/builtins/ft_command_unset.c
@@ -6,31 +6,8 @@
 /*   By: ewolfghe <ewolfghe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 14:30:51 by ewolfghe          #+#    #+#             */
-/*   Updated: 2023/04/04 19:20:05 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:22:45 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-void	ft_command_unset(char **tokens)
-{
-	char	**ep;
-	char	**p;
-
-	ep = NULL;
-	while (*ep != NULL)
-	{
-		if (strncmp(tokens[1], *ep, strlen(tokens[1])) == 0 && \
-					(*ep)[strlen(tokens[1])] == '=')
-		{
-			p = ep;
-			while (*p != NULL)
-			{
-				*p = *(p + 1);
-				p++;
-			}
-		}
-		else
-			ep++;
-	}
-}
