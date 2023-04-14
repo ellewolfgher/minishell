3afa8d58bd34de525e98c0a6a8a593ddf8f13f22
renanguillen <rigcpj@gmail.com>
2023-04-14 14:03:43 -0300

    getting pipes to work

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:39:08 by ridalgo-          #+#    #+#             */
-/*   Updated: 2023/04/13 10:51:49 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2023/04/14 11:40:52 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,7 +142,11 @@ int			ft_searchset_pipe_in(t_data *ms);
 int			ft_searchset_pipe_out(t_data *ms);
 
 int			ft_execute_builtin(t_execute *command, t_data *ms, int og_fds[2]);
-int			ft_execute_loop(t_execute *command, t_data *ms, int ogfds[2]);
+int			ft_execute_command(t_execute *command, t_data *ms, int og_fds[2]);
+pid_t		ft_execute_fork(void);
+int			ft_execute_get_error(char *path, t_data *ms);
+int			ft_execute_loop(t_execute *command, t_data *ms, int og_fds[2]);
+int			ft_execute_multiple(t_execute *command, t_data *ms, int og_fds[2]);
 void		ft_execute_output_create(t_execute *command);
 int			ft_execute_redirects(t_execute *command, int og_fds[2], t_data *ms);
 t_execute	*ft_execute_set_commands(t_data *ms);
---
+++ sources/execute/ft_execute_command.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execute_command.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 14:54:06 by ridalgo-          #+#    #+#             */
+/*   Updated: 2023/04/14 12:14:45 by ridalgo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/*
+Executes the given command in a new process, sets the exit code in the t_data
+structure, and returns the exit code as an integer. If the command is not valid
+or not executable, sets an appropriate error code and prints an error message.
+
+Example:
+Imagine that the command is "ls -l /tmp" and that the command is valid and
+executable. If the command is executed successfully, exit_code will store the
+exit code.
+*/
+int	ft_execute_command(t_execute *command, t_data *ms, int og_fds[2])
+{
+	int	pid;
+
+	(void)og_fds;
+	pid = ft_execute_fork();
+	if (!pid)
+	{
+		ms->exit_code = 0;
+		if (!ft_execute_get_error(command->command, ms))
+		{
+			ft_signals_default();
+			// if (ft_execute_redirects(command, og_fds, ms))
+			// {
+			// 	ms->need_to_exit = -1;
+			// 	return (ft_fds_restore(og_fds));
+			// }
+			execve(command->command, command->args, command->envp);
+		}
+		else
+			ms->need_to_exit = -1;
+		return (ms->exit_code);
+	}
+	return (ms->exit_code);
+}
