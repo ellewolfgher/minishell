d167d7c84bf4a4fc67ba082b96e51510eb8e510b
renanguillen <rigcpj@gmail.com>
2023-04-15 18:45:56 -0300

    few modifications

--- sources/parser/tokens/ft_token_error.c
+++ sources/parser/tokens/ft_token_error.c
@@ -6,26 +6,26 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 18:30:32 by ridalgo-          #+#    #+#             */
-/*   Updated: 2023/04/03 18:46:57 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2023/04/15 17:56:50 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
 //Checks if no rules are broken, returns 1 if there is an error
-int	ft_token_error(t_tokens *temp)
+int	ft_token_error(t_tokens *aux)
 {
-	if (temp->type == OPTOKEN && (!ft_strcmp(temp->value, "||")))
+	if (aux->type == OPTOKEN && (!ft_strcmp(aux->value, "||")))
 		return (1);
-	if (temp->type == OPTOKEN && !temp->prev)
+	if (aux->type == OPTOKEN && !aux->prev)
 		return (1);
-	if (temp->prev && temp->prev->type == REDTOKEN)
-		if (temp->type == OPTOKEN || temp->type == REDTOKEN)
+	if (aux->prev && aux->prev->type == REDTOKEN)
+		if (aux->type == OPTOKEN || aux->type == REDTOKEN)
 			return (1);
-	if (temp->prev && temp->prev->type == OPTOKEN)
-		if (temp->type == OPTOKEN)
+	if (aux->prev && aux->prev->type == OPTOKEN)
+		if (aux->type == OPTOKEN)
 			return (1);
-	if ((temp->type == REDTOKEN || temp->type == OPTOKEN) && !temp->next)
+	if ((aux->type == REDTOKEN || aux->type == OPTOKEN) && !aux->next)
 		return (1);
 	return (0);
 }
