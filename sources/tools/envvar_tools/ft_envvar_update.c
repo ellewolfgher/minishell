969e2f67d09acadff53674eddbbc6b4d57ed395d
Elle Wolfgher <ellewolfgher@gmail.com>
2023-04-21 01:37:38 -0300

    leak fix

--- sources/tools/envvar_tools/ft_envvar_update.c
+++ sources/tools/envvar_tools/ft_envvar_update.c
@@ -6,7 +6,7 @@
 /*   By: ewolfghe <ewolfghe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:39:35 by ewolfghe          #+#    #+#             */
-/*   Updated: 2023/04/19 18:09:43 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2023/04/20 22:36:40 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,17 @@ static void	ft_envvar_replace(char *name, char *value, t_env_vars **env_vars)
 {
 	t_env_vars	*node;
 	char		*new_content;
+	char		*equal_val;
 
 	node = ft_envvar_find(name, *env_vars);
 	if (ft_strcmp(value, "") == 0 || !value)
 		new_content = ft_strdup(name);
 	else
-		new_content = ft_strjoin(name, ft_strjoin("=", value));
+	{
+		equal_val = ft_strjoin("=", value);
+		new_content = ft_strjoin(name, equal_val);
+		ft_free((void **)&equal_val);
+	}
 	if (node)
 	{
 		ft_free((void **)&node->content);
@@ -69,7 +74,7 @@ void	ft_envvar_update(char *name, char *value, t_env_vars **env_vars)
 	{
 		join_new = ft_strjoin(var_str, value);
 		ft_envvar_back(env_vars, ft_envvar_new(join_new));
+		ft_free((void **)&join_new);
 	}
-	ft_free((void **)&join_new);
 	ft_free((void **)&var_str);
 }
